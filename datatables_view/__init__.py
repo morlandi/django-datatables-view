7d0aef357b029758a3c6636f458443d2b5823682
Mario Orlandi <morlandi@brainstorm.it>
2018-06-23 09:39:39 +0200

    package version added

--- datatables_view/__init__.py
+++ datatables_view/__init__.py
@@ -1,3 +1,6 @@
+from __future__ import unicode_literals
+__version__ = '0.0.1'
+
 
 from .columns import (
     Column,
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
+import os
+import re
 from setuptools import find_packages, setup
 
+
+def get_version(*file_paths):
+    """Retrieves the version from specific file"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+version = get_version("datatables_view", "__init__.py")
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
 setup(name='django-datatables-view',
-      version='0.0.1',
+      version=version,
       description='Helper class to integrate Django with datatables',
+      long_description=readme + '\n\n' + history,
       url='http://github.com/morlandi/django-datatables-view',
       author='Mario Orlandi',
       author_email='morlandi@brainstorm.it',
