186885704a340aec6cfca5876d6d8fb37dab74f5
Mario Orlandi <morlandi@brainstorm.it>
2019-03-15 16:07:17 +0100

    Fix "AttributeError: 'datetime.date' object has no attribute 'tzinfo'" when receiving a datetime.date object

--- datatables_view/utils.py
+++ datatables_view/utils.py
@@ -1,4 +1,5 @@
 import pprint
+import datetime
 from django.utils import timezone
 from django.conf import settings
 from django.utils import formats
@@ -37,11 +38,15 @@ def format_datetime(dt, include_time=True):
     if dt is None:
         return ''
 
-    try:
-        dt = timezone.localtime(dt)
-    except:
-        local_tz = pytz.timezone(getattr(settings, 'TIME_ZONE', 'UTC'))
-        dt = local_tz.localize(dt)
+    if isinstance(dt, datetime.datetime):
+        try:
+            dt = timezone.localtime(dt)
+        except:
+            local_tz = pytz.timezone(getattr(settings, 'TIME_ZONE', 'UTC'))
+            dt = local_tz.localize(dt)
+    else:
+        assert isinstance(dt, datetime.date)
+        include_time = False
 
     use_l10n = getattr(settings, 'USE_L10N', False)
     text = formats.date_format(dt, use_l10n=use_l10n, format='SHORT_DATE_FORMAT')
