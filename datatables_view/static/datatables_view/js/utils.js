7dde9b407a6aff7f3b123efc95d1c3a59a105831
Thierry BOULOGNE <tboulogne@tng-concepts.com>
2019-07-08 14:48:11 +0200

    Add getCookie
    getCookie is called on line 296 but missing.

--- datatables_view/static/datatables_view/js/utils.js
+++ datatables_view/static/datatables_view/js/utils.js
@@ -87,7 +87,19 @@ window.DatatablesViewUtils = (function() {
         }
     };
 
-
+    function getCookie(cname) {
+        var name = cname + "=";
+        var ca = document.cookie.split(';');
+        for(var i=0; i<ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0)==' ') c = c.substring(1);
+            if(c.indexOf(name) == 0)
+            return c.substring(name.length,c.length);
+        }
+        return "";
+    }
+    
+    
     function _setup_column_filters(table, data) {
 
         if (data.show_column_filters) {
